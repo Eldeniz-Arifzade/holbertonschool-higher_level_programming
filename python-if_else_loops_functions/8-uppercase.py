6cede66dedd22f51807e8b096801aafe7f001d1f
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-11-30 12:10:20 +0000

    delete 3rd print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     for char in str:
         if 65 <= ord(char) <= 90:
-            print('{}'.format(str), end='')
+            print('{}'.format(str), end='' if char != str[-1] else ' ')
         elif 97 <= ord(char) <= 122:
-            print('{}'.format(chr(ord(char)-32)), end='')
-    print(' ')
+            print('{}'.format(chr(ord(char)-32)), end='' if char != str[-1] else ' ')
