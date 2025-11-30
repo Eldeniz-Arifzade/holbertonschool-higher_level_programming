7281a1d2e7083b48d5014528b420996acd35a18a
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-11-30 12:22:14 +0000

    change n to str

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in range(len(str)):
         if 97 <= ord(str[i]) <= 122:
-            print('{}'.format(chr(ord(str[i]) - 32)), end='' if i < len(n)-1 else '\n')
+            print('{}'.format(chr(ord(str[i]) - 32)), end='' if i < len(str)-1 else '\n')
         else:
-            print('{}'.format(str[i]), end='' if i < len(n) - 1 else '\n')
+            print('{}'.format(str[i]), end='' if i < len(str) - 1 else '\n')
