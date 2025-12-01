f3615fa2e20b63789096b43ab86df318c8a5ca40
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-01 08:49:28 +0000

    infinite add

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ = '__main__':
+    from sys import argv
+    s = 0
+    for i in argv:
+        if i == 0:
+            continue
+        s += int(argv[i])
+    print(s)
