f8db94b8ac021725019cab83d0a9bf82d9a2b059
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-01 10:21:13 +0000

    advanced 1

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
     elif argv[2] == '+':
-        print('{} + {} = {}'.format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], add(int(argv[1]), int(argv[3]))))
     elif argv[2] == '-':
-        print('{} - {} = {}'.format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], sub(int(argv[1]), int(argv[3]))))
     elif argv[2] == '*':
-        print('{} * {} = {}'.format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == '/':
-        print('{} / {} = {}'.format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], div(int(argv[1]), int(argv[3]))))
     else:
         print('Unknown operator. Available operators: +, -, * and /\n')
     sys.exit(1)
