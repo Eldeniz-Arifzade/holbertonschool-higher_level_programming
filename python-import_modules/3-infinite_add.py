c9ffcfaf29b73802d7c2ef2955290279f52bbce0
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-01 08:52:18 +0000

    fix error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     from sys import argv
     s = 0
     for i in argv:
-        if i == 0:
+        if i == argv[0]:
             continue
         s += int(i)
     print(s)
