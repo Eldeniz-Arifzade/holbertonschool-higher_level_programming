9453e571ab8234dceca112a16f32cc321b97dde8
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-01 10:33:09 +0000

    fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,11 +5,9 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    
     a = argv[1]
     operator = argv[2]
     b = argv[3]
-
     if operator == '+':
         print('{} {} {} = {}'.format(a, operator, b, add(int(a), int(b))))
     elif argv[2] == '-':
