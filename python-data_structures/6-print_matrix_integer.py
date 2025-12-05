53de512d91c182a5d0dee63ee9f5a32af2253105
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-05 21:20:37 +0000

    check the last idx in each row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for entry in row:
-            print('{:d}'.format(entry), end=' ')
+        for entry_idx in range(len(row)):
+            if entry == len(row) - 1:
+                print('{:d}'.format(entry))
+            else:
+                print('{:d}'.format(entry), end=' ')
         print()
