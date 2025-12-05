76ebc02b76f1b01b1384fdcdd52d2e858d70dc74
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-05 21:28:38 +0000

    change end

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for entry_idx in range(len(row)):
             if entry_idx == len(row) - 1:
-                print('{:d}'.format(row[entry_idx]))
+                print('{:d}'.format(row[entry_idx]), end='')
             else:
                 print('{:d}'.format(row[entry_idx]), end=' ')
         print()
