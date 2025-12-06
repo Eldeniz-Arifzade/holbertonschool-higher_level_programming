7a00068f627fdd7d7541380c46692c7ec57870c6
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-06 21:55:30 +0000

    fix

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print({key: a_dictionary[key] for key in sorted(a_dictionary)})
+    sorted_keys = sorted(a_dictionary)
+    for key in sorted_keys:
+        print(f'{key}: {a_dictionary[key]}')
