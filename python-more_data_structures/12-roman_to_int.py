8ff9d50ed4bb81480e91b7102503933fd5919bc7
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-06 22:59:03 +0000

    add last num

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if len(roman_string) is None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) != str:
         return 0
     s = 0
     for i in range(len(roman_string)-1):
@@ -9,4 +9,5 @@ def roman_to_int(roman_string):
             s -= roman[roman_string[i]]
         else:
             s += roman[roman_string[i]]
+    s += roman[roman_string[-1]]
     return s
