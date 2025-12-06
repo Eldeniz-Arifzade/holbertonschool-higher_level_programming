926b5a75270f6092415988e00aada75920729e26
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-06 23:01:17 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string is None or roman_to_int is not str:
+    if roman_string is None or type(roman_string) != str:
         return 0
     s = 0
     for i in range(len(roman_string)-1):
