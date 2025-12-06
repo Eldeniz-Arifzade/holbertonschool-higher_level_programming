58f782aaf239a4815d7b1c8a55d897c9fc077584
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-06 23:06:30 +0000

    consider 0 case

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     sum_weights = 0
-    s = 0
+    s = 
+    if my_list is None:
+        return 0
     for t in my_list:
         s += t[0] * t[1]
         sum_weights += t[1]
