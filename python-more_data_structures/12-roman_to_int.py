e4a2693a0f6864c1903e857bdbc43e9b37e403f5
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-06 22:52:03 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_list = list(roman_string)
-    if roman_string is None or type(roman_string) != str:
+    if len(roman_string) is None or type(roman_string) != str:
         return 0
-    if len(roman_list) == 1:
-        return roman[roman_list[0]]
-    for i in range(0, len(roman_list)-1):
-        if roman[roman_list[i]] >= roman[roman_list[i+1]]:
-            roman_list[i+1] = roman_list[i] + roman_list[i+1]
+    s = 0
+    for i in range(len(roman_list)-1):
+        if roman[roman_string[i]] < roman[roman_string[i+1]]:
+            s -= roman[roman_string[i]]
         else:
-            roman_list[i+1] = roman_list[i+1] - roman_list[i]
-    return roman_list[-1]
+            s += roman[roman_string[i]]
+    return s
