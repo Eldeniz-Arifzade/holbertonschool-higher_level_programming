7c2192f3cf37ad62f0b5bf0b5d20819d4c9c189d
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-07 18:14:06 +0000

    gitpusher

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,9 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             count += 1
     except IndexError:
-        count = 0
-        for i in my_list:
-            print(i, end='')
-            count += 1
+        pass
     print()
     return count
