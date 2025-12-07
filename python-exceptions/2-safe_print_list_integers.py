d916605094efdd3f4cd6ca95a52c0c5d85c4efac
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-07 18:36:50 +0000

    gitpusher

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in my_list:
         if count >= x:
+            raise IndexError
             break
         try:
             print("{:d}".format(i), end='')
             count += 1
         except (TypeError, ValueError):
             pass
-        except IndexError as e:
-            raise IndexError
     print()
     return count
