788774b87fefb9d55f03c8d5b8e3932fca71350f
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-07 18:06:48 +0000

    correct except clase

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             count += 1
     except IndexError:
-        count = 0
-        for i in my_list:
+        for i in range(count, x):
             print(i, end='')
             count += 1
     print()
