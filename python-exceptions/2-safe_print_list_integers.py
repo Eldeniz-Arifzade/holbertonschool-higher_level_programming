53b9af034d67c62c2d0d648efce748432337e579
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-07 18:30:35 +0000

    fix count

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in my_list:
-        if count > x:
+        if count >= x:
             break
         try:
             print("{:d}".format(i), end='')
