c9f1d86670dec6368b3e202980755442d3906442
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-07 19:37:47 +0000

    advanced task 1

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as e:
+        sys.stderr.write(f"Exception: {raise e}")
