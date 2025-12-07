fcc3468745673e6bba6464ecb4ccbc704af7740b
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-07 18:31:59 +0000

    raise error for index

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (TypeError, ValueError):
             pass
+        except IndexError:
+            raise
     print()
     return count
