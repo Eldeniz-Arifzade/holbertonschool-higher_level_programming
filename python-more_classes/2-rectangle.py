30e96a6bd2e2dbe09f9cce659137febd062c2fb7
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-10 09:12:20 +0000

    return 0 if one side is 0 in P

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -43,4 +43,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calc the perimeter based on width and height"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
