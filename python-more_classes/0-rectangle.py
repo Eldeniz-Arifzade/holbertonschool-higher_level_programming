329cd6235aa668d7355734758874265ad0fa044e
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-10 08:39:50 +0000

    write empty class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module will contain a class names Rectangle"""
+
+
+class Rectangle:
+    """This class will define a Rectangle"""
+    def __init__(self):
+        """Initialize empty class"""
+        pass
