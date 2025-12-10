b9348ffeb1a052be6ac1170111843658bda5152f
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-10 09:33:47 +0000

    shotening the ling

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -43,7 +43,10 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return rect_print
         for i in range(self.__height):
-            rect_print += '#' * self.__width + '\n' if i != self.__height - 1 else '#' * self.__width
+            if i != self.__height - 1:
+                rect_print += '#' * self.__width + '\n'
+            else:
+                rect_print += '#' * self.__width
         return rect_print
 
     def area(self):
