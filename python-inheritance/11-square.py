b5349b42980dfcf977d8d7dd9059ff7cb017d5b2
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 21:10:37 +0000

    add print featgure

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module will define class Square inherited from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherited from Rectangle"""
+    def __init__(self, size):
+        """Initialize the class"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Implement area for Square class"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Specify print option for Square class"""
+        return f"[Square] {self.__size}/{self.__size}"
