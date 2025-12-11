11585a0334e73b8176700ba65c96b7f0aadab23f
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:08:25 +0000

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module will define a class MyList which will inherit list"""
+
+
+class MyList(list):
+    """This class will inherit from list and define one public method"""
+    def __init__():
+        """initialize class"""
+        super.__init__(*args)
+
+    def print_sorted(self):
+        return sorted(self)
