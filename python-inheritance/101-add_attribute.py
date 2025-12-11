7881654b1ae6263a9e0299c5bffa99a307ca18ed
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 21:29:17 +0000

    change parameter names

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,7 +2,7 @@
 """This module will define a function for adding attribute too object"""
 
 
-def add_attribute(obj, attr_name, attr_value):
+def add_attribute(obj, name, value):
     """Function for adding attribute"""
     if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
