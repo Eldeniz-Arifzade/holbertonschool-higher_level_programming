496bd10e2339abd4c6acf799e0a3b415ad78b362
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:28:10 +0000

    subclass

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Check if class is inherited from some class"""
+
+
+def inherits_from(obj, a_class):
+    """Function for checking classes"""
+    return issubclass(obj, a_class)
