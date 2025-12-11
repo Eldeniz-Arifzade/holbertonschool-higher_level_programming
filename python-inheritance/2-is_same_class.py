b188cc7558c34631361aa5963ce1c95d24ff91fe
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:21:50 +0000

    isinstance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module will define a function for checking class instances"""
+
+
+def is_same_class(obj, a_class):
+    """Function for checking classes"""
+    return isinstance(obj, a_class)
