e5ea2820bb52c5a2af955843389a18928cdc4c83
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:35:22 +0000

    area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module will define empty class"""
+
+
+class BaseGeometry:
+    """Define the geometry with one method class"""
+    def area(self):
+        """Function for calculating area"""
+        raise Exception("area() is not implemented")
