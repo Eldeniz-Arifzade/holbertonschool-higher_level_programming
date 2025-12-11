fcbd2af08aca30b012a3ecdcf595aa3e0e344417
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:26:53 +0000

    use isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Check if class is instance of inherited from some class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function for checking classes"""
+    return isinstance(obj, a_class)
