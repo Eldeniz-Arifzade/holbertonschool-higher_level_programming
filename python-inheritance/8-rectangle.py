b851838a4d5251b1d3b99fc9325b8e1d463912de
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:58:46 +0000

    add one more blank line

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """This module will define a class Rectangle that will inherit from BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
     def __init__(self, width, height):
