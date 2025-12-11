d27cfe1f38ffa4bd3aef639bdcee064f4359d1d6
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 20:45:11 +0000

    fix the type

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,4 +12,4 @@ class BaseGeometry:
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
-            raise ValueError(f"{name} must be great than 0")
+            raise ValueError(f"{name} must be greater than 0")
