5d5298dffa6ed3a140d335327bb33c705cc4f0a5
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-11 21:02:59 +0000

    implemnt area and add print option

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module will define a class that will inherit from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Initialize the class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Implement area method"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """How instances will show up when printed"""
+        return f"[Rectangle] {self.__width/self.__height}"
