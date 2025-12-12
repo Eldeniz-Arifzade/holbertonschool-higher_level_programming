788d925ceebebe09e9fa3e308bad39890b859e53
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-12 18:52:34 +0000

    read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module will define a function for reading a text file"""
+
+
+def read_file(filename=""):
+    """Function for reading and printing file content"""
+    with open(filename, 'r', encoding='UTF8') as f:
+        print(f.read())
