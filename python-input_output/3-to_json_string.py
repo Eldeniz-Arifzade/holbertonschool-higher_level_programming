df717093f48a4f2eb9a8563aecceb9b8df415905
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-12 19:06:59 +0000

    add blank line

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Module will define a function which will return JSON representations of str"""
 import json
 
+
 def to_json_string(my_obj):
     """Function for finding json representation of my_obj"""
     return json.dumps(my_obj)
