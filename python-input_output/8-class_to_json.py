a9a1b44c714f6b0f2b181a05d8467b910b03449c
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-12 19:45:37 +0000

    remove json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Class to JSON"""
-import json
 
 
 def class_to_json(obj):
     """Function that will convert class to JSON"""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
