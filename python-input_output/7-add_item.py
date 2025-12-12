52817a50409b66fb4a3fa7c0ca3202a549ea38b2
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-12 19:28:52 +0000

    use __import__

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module will load, add, and save args to file"""
 from sys import argv
-from '5-save_to_json_file' import save_to_json_file
-from '6-load_from_json_file' import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from 6-load_from_json_file import load_from_json_file
 save_to_json_file(argv, 'add_item.json')
 load_from_json_file('add_item.json')
