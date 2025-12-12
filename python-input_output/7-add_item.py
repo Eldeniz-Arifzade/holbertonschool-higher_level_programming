05e95d1ac8f2d1405a8cec256828f4f2ea2b3a48
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-12 19:31:10 +0000

    gitpusher

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,5 +3,5 @@
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file(argv, 'add_item.json')
 load_from_json_file('add_item.json')
+save_to_json_file(argv[1:], 'add_item.json')
