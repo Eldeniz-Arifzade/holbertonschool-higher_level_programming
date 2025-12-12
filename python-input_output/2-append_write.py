88944fee23162d235dd9e901301f77120e0a3504
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-12 19:02:24 +0000

    change func name

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """This module will define a function which will append to a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Function will append text to file, and return num of chars written"""
     with open(filename, 'a', encoding='UTF8') as f:
         return f.write(text)
