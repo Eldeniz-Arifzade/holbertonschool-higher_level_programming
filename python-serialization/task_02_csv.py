3cb90ebe2b72255bea010da383c0fa24c6b64e2a
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-17 12:11:39 +0000

    change file name

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -6,7 +6,7 @@ def convert_csv_to_json(filename):
     try:
         with open(filename, 'r') as file:
             with open('data.json', 'w') as data:
-                json.dump(list(csv.DictReader(file)), file)
+                json.dump(list(csv.DictReader(file)), data)
                 return True
     except Exception:
         return False
