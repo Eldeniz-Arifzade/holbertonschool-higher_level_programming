db135d0158d0b2edb95702e5913790d700d7829f
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-17 11:56:27 +0000

    pickle module

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, 'w') as file:
+            pickle.dump(self.__dict__.copy(), file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'r') as file:
+            try:
+                return pickle.load(filename)
+            except Exception:
+                return None
