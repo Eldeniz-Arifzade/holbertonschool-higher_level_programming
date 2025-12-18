ef703ce0b9c2b3b62d664fc31d36458d569ad0af
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-18 18:57:01 +0000

    xml first time

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        element = ET.SubElement(root, key)
+        element.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='UTF-8', xml_decoration=True)
+
+def deserialize_from_xml(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    result = {}
+    for element in root:
+        text = element.text
+        if text is None:
+            value = None
+        elif text.isdigit():
+            value = int(text)
+        else:
+            try:
+                value = float(text)
+            except ValueError:
+                value = text
+        result[element.tag] = value
+    return result
